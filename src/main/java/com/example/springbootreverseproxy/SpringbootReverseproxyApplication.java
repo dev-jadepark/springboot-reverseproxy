77379b41cf3ff9ee5c64e9b5c1b46ca5af638388
parkforte <parkforte@naver.com>
2022-07-25 11:54:47 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootreverseproxy/SpringbootReverseproxyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreverseproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReverseproxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootReverseproxyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootreverseproxy/SpringbootReverseproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootreverseproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReverseproxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
