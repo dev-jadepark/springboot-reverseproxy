181d7751f52dc9e9feaf341ea4a7c9f5b718f193
Jade Park <dev.jadepark@gmail.com>
2022-12-08 10:52:49 +0900

    test

--- src/main/java/com/example/springbootreverseproxy/api/RootController.java
+++ src/main/java/com/example/springbootreverseproxy/api/RootController.java
@@ -2,6 +2,7 @@ package com.example.springbootreverseproxy.api;
 
 import com.example.springbootreverseproxy.common.ResultVo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,21 +10,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class RootController {
 
     private final Environment env;
 
 
-    final Date date = new Date();
+
 
     @GetMapping("/")
     public ResponseEntity<ResultVo> helloWorld(){
-        System.out.println( date.getTime() +" request");
+        LocalTime localTime = LocalTime.now();
+
+        int HH = localTime.getHour();
+        int mm = localTime.getMinute();
+        int ss = localTime.getSecond();
+        System.out.println( HH + ":" + mm + ":" + ss + " request");
         ResultVo resultVo = ResultVo.builder().data(Arrays.toString(env.getActiveProfiles()) + " WAS Hello World").build();
 
         return new ResponseEntity<>(resultVo, HttpStatus.OK);
@@ -32,7 +43,12 @@ public class RootController {
 
     @GetMapping("/test")
     public ResponseEntity<ResultVo> helloWorldtest(){
-        System.out.println( date.getTime() +" request");
+        LocalTime localTime = LocalTime.now();
+
+        int HH = localTime.getHour();
+        int mm = localTime.getMinute();
+        int ss = localTime.getSecond();
+        System.out.println( HH + ":" + mm + ":" + ss + " request");
         ResultVo resultVo = ResultVo.builder().data(Arrays.toString(env.getActiveProfiles()) + " WAS Hello World").build();
         return new ResponseEntity<>(resultVo, HttpStatus.OK);
     }
